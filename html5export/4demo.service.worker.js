1cf5742a5fd69003d732d9a5abc413c5f6cb980d
PLAZMOIDENKO <call.of.plazma@gmail.com>
2025-06-29 18:26:43 +0200

    новый апдейт

--- html5export/4demo.service.worker.js
+++ html5export/4demo.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751201017|17407973961';
+const CACHE_VERSION = '1751214393|30743358524';
 /** @type {string} */
 const CACHE_PREFIX = 'Go Race: REDUX-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
