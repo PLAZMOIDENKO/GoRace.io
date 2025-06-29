63899f7ac9d99d44ecc2ecb47f3a0a0397166323
PLAZMOIDENKO <call.of.plazma@gmail.com>
2025-06-29 22:29:04 +0200

    обновление

--- html5export/index.service.worker.js
+++ html5export/index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751214444|30794448372';
+const CACHE_VERSION = '1751228811|45107407988';
 /** @type {string} */
 const CACHE_PREFIX = 'Go Race: REDUX-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = '4demo.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["4demo.html","4demo.js","4demo.offline.html","4demo.icon.png","4demo.apple-touch-icon.png","4demo.audio.worklet.js","4demo.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["4demo.wasm","4demo.pck"];
+const CACHEABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
